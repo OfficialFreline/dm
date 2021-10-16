038ae06ba7e8dd17bedb01a518723b1a2bd0af23
Freline <49955245+OfficialFreline@users.noreply.github.com>
2021-10-16 16:04:02 +0300

    Rounding out the design

--- gamemode/core/interface/vgui/dscrollpanel_cl.lua
+++ gamemode/core/interface/vgui/dscrollpanel_cl.lua
@@ -1,11 +1,11 @@
 local PANEL = {}
 
 function PANEL:Init()
-	self.VBar:SetWide( 12 )
+	self.VBar:SetWide( 14 )
 	self.VBar:SetHideButtons( true )
 	self.VBar.Paint = nil
 	self.VBar.btnGrip.Paint = function( self, w, h )
-		draw.RoundedBox( 0, 4, 4, w - 8, h - 8, self.Depressed and DMColor.sp_hov or DMColor.sp )
+		draw.RoundedBox( 4, 4, 4, w - 8, h - 8, self.Depressed and DMColor.sp_hov or DMColor.sp )
 	end
 end
 
